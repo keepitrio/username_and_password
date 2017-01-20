43438bc9d0ac94520a65397e93014b34d4abad2d
Rio <rnose512@gmail.com>
2017-01-20 07:23:33 -0800

    worked on JS file but not working as planned

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-function checkPassword() {
-  //This variable should be the content of what's inputted into password
-  var password = el.value;
-  //This variable should be the content of what's inputted into password_check
-  var password_check =
-  //if password != password_check, show message that passwords do not match.
-  //also disable the submit button.
-  //if password === password_check, show no message.
-  if
-}
-
-function submitMessage() {
-  window.alert("Thank you for entering a username and password!");
-}
-
 var elSubmit = document.getElementById('submit');
-elSubmit.addEventListener('click', submitMessage, true);
+  var pw = document.getElementById('password').value;
+  var pwch = document.getElementById('password_check').value;
+
+elSubmit.addEventListener('click', function(e) {
+
+  if (pw !== pwch) {
+    window.alert("Passwords do not match. Please try again.");
+  } else {
+  window.alert("Thank you for entering a username and password!");
+  }
+});
